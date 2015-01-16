f6f845381fd16dffd7773b164e0971077d900524
Ryan Dwyer <ryanpdwyer@gmail.com>
2015-01-16 11:33:02 -0500

    Minor changes to setup.py for readability, cross-platform

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import subprocess
 # Using setuptools enables the package to be used in develop mode
 # See http://pythonhosted.org/setuptools/setuptools.html#development-mode
 # See https://github.com/scikit-learn/scikit-learn/issues/1016
@@ -21,6 +20,10 @@ versioneer.parentdir_prefix = 'jittermodel-' # dirname like 'myproject-1.2.0'
 
 
 # ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+
 # This section taken from
 # http://nfoti.github.io/a-creative-blog-name/posts/2013/02/07/cleaning-cython-build-files/
 
@@ -29,11 +32,16 @@ args = sys.argv[1:]
 # Make a `cleanall` rule to get rid of intermediate and library files
 if "cleanall" in args:
     print("Deleting cython files...")
-    # Just in case the build directory was created by accident,
-    # note that shell=True should be OK here because the command is constant.
-    subprocess.Popen("rm -rf build", shell=True, executable="/bin/bash")
-    subprocess.Popen("rm -rf jittermodel/*.c", shell=True, executable="/bin/bash")
-    subprocess.Popen("rm -rf jittermodel/*.so", shell=True, executable="/bin/bash")
+    import glob
+    import shutil
+    import os.path
+    shutil.rmtree('build')
+
+    for filename in glob.glob(os.path.join('jittermodel','*.c')):
+        os.remove(filename)
+
+    for filename in glob.glob(os.path.join('jittermodel','*.so')):
+        os.remove(filename)
 
     # Now do a normal clean
     sys.argv[1] = "clean"
@@ -41,8 +49,6 @@ if "cleanall" in args:
 # We want to always use build_ext --inplace
 if args.count("build_ext") > 0 and args.count("--inplace") == 0:
     sys.argv.insert(sys.argv.index("build_ext")+1, "--inplace")
-
-# ------------------------------------------------------------------------------
 
 # See http://stackoverflow.com/a/18034855/2823213
 # If the user does not have Cython installed, we should be able to use the
@@ -63,6 +69,7 @@ def make_extension(name, have_cython):
     return Extension(name, [name.replace('.', '/')+ext])
 
 # List of cython modules here. Make_extension will add the correct extension.
+# Assumes the file lives at pkgname/modulename
 extensions = ['jittermodel._sim']
 ext_modules = [make_extension(name, have_cython) for name in extensions]
 
@@ -88,7 +95,7 @@ setup(name='jittermodel',
         'Programming Language :: Python :: 2.7'],
       author_email='ryanpdwyer@gmail.com',
       url='http://github.com/ryanpdwyer/jittermodel',
-      packages=['jittermodel', 'jittermodel.tests'],
+      packages=find_packages(),
       zip_safe=False,
       install_requires=['numpy', 'scipy', 'matplotlib', 'pint'],
       tests_require=['mpmath', 'bunch', 'nose'],
